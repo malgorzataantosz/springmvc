220f59809e772b96eac4f1e8671a75308ebf7c28
malgorzataantosz <32096240+malgorzataantosz@users.noreply.github.com>
2017-10-02 12:25:15 +0200

    Update ValidEmail.java

--- src/springmvc/spring/web/validation/ValidEmail.java
+++ src/springmvc/spring/web/validation/ValidEmail.java
@@ -13,21 +13,6 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-/**
- * The annotated element size must be between the specified boundaries (included).
- * <p/>
- * Supported types are:
- * <ul>
- *     <li>{@code CharSequence} (length of character sequence is evaluated)</li>
- *     <li>{@code Collection} (collection size is evaluated)</li>
- *     <li>{@code Map} (map size is evaluated)</li>
- *     <li>Array (array length is evaluated)</li>
- * </ul>
- * <p/>
- * {@code null} elements are considered valid.
- *
- * @author Emmanuel Bernard
- */
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
 @Retention(RUNTIME)
 @Documented
